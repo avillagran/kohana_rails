aa63f0e4d268846660a13c52e097d020a31a634e
Andrés Villagrán <andres@villagranquiroz.cl>
2011-09-09 16:48:04 -0300

    Static date helper

--- classes/datehelper.php
+++ classes/datehelper.php
@@ -1,7 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class DateHelper {
-	function parse_date($my_sql_string_date, $default_format) 
+		
+	public static function parse_date($my_sql_string_date, $default_format) 
 	{
 		list($date, $time) = explode(" ", $my_sql_string_date);
 		list($year, $month, $day) = explode("-", $date);
