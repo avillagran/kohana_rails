33414685c231587c0a2bc893b8b011be36107404
Andrés Villagrán <andres@villagranquiroz.cl>
2012-01-10 16:21:25 -0300

    Timezone_date fix

--- classes/datehelper.php
+++ classes/datehelper.php
@@ -20,7 +20,7 @@ class DateHelper {
 	
     public static function timezone_date($mysql_string_date, $str_format = "%Y-%m-%d %H:%M:%S")
     {
-        @list($date, $time) = explode(" ", $my_sql_string_date);
+        @list($date, $time) = explode(" ", $mysql_string_date);
         @list($year, $month, $day) = explode("-", $date);
         @list($hour, $minute, $second) = explode(":", $time);
         @$unix_stamp = mktime($hour, $minute, $second, $month, $day, $year);
