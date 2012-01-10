7693a927a3aefd70a26454654b5f877d664c35dd
Andrés Villagrán <andres@villagranquiroz.cl>
2012-01-10 12:49:09 -0300

    Fix datetime

--- classes/datehelper.php
+++ classes/datehelper.php
@@ -18,4 +18,13 @@ class DateHelper {
         return date($str_format, $unix_stamp);        
     }
 	
+    public static function timezone_date($mysql_string_date, $str_format = "%Y-%m-%d %H:%M:%S")
+    {
+        @list($date, $time) = explode(" ", $my_sql_string_date);
+        @list($year, $month, $day) = explode("-", $date);
+        @list($hour, $minute, $second) = explode(":", $time);
+        @$unix_stamp = mktime($hour, $minute, $second, $month, $day, $year);
+
+        return strftime( $str_format, $unix_stamp );
+    }
 }
